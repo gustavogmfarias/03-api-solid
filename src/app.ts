d6883c545a56b715798c36a529852cd729cd647e
Gustavo <gustavogmfarias@gmail.com>
2023-03-17 14:35:49 -0300

    .

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,6 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "@/http/routes";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
-
-prisma.user.create({
-  data: {
-    name: "Diego Fernandes",
-    email: "diego@rocketsaeet.com.br",
-  },
-});
-
-// ORM - Object Relational Mapper
+app.register(appRoutes);
